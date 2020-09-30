834b8181c2983eda965fca609218de0e588dccbe
Shadab Khan <shadab.2005@gmail.com>
2020-10-01 01:03:37 +0530

    Add app bar and add router and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import Web3 from 'web3';
-import Header from './components/Header';
-import Content from './components/Content';
-import './css/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './theme';
+import AppBar from './components/appBar/AppBar';
 
 const App: React.FC = () => {
-  return(
-    <div>
-        <Header />
-        <Content />
-      </div>);
+  return (
+    <ThemeProvider theme={theme}>
+      <main>
+        <Router>
+          <AppBar />
+          <Switch>
+          </Switch>
+        </Router>
+      </main>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/components/appBar/AppBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = styled.div`
+  width: 100%;
+  height: 80px;
+`;
+
+const AppBar: React.FC<{}> = () => {
+  return <Main>
+  </Main>;
+}
+
+export default AppBar;
