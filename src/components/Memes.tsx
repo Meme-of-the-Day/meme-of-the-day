96c81139d653890ec028416d163373c00e617fed
Shadab Khan <shadab.2005@gmail.com>
2020-11-29 02:10:33 +0530

    Meme design

--- src/components/Memes.tsx
+++ src/components/Memes.tsx
@@ -1,17 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Meme from './Meme';
-import { Textile } from '../utils/textile';
-import { MemeMetadata } from '../utils/Types'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Meme from "./Meme";
+import { Textile } from "../utils/textile";
+import { MemeMetadata } from "../utils/Types";
 
 const Main = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-
-  @media screen and (min-width: 1280px) {
-    justify-content: flex-start;
-  }
+  flex-direction: column;
+  align-items: center;
+  overflow: auto;
 `;
 
 const CustomMeme = styled(Meme)`
@@ -19,7 +16,7 @@ const CustomMeme = styled(Meme)`
   width: 100%;
 
   @media screen and (min-width: 768px) {
-    width: 48%;
+    width: 60%;
   }
 
   @media screen and (min-width: 1280px) {
@@ -35,17 +32,16 @@ const Memes: React.FC<{}> = () => {
       const textile = await Textile.getInstance();
       const memes = await textile.getAllMemes();
       setMemeMetadata(memes);
-    }
+    };
     init();
   }, []);
 
   return (
     <Main>
-      {
-        memeMetadata && memeMetadata.map((meme) => <CustomMeme meme={meme} key={meme.cid} />)
-      }
+      {memeMetadata &&
+        memeMetadata.map(meme => <CustomMeme meme={meme} key={meme.cid} />)}
     </Main>
-  )
-}
+  );
+};
 
 export default Memes;
