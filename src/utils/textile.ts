6d304a8fd2957c1b2114d65d78cbe1cac346ea44
Anmol Agarwala <johnanmol3405@outlook.com>
2020-12-01 00:06:18 +0530

    fix env var in textile

--- src/utils/textile.ts
+++ src/utils/textile.ts
@@ -32,7 +32,7 @@ export class Textile {
   private async init() {
     const env = process.env.NODE_ENV;
     this.hubAuthURL = env !== 'production' ? process.env.REACT_APP_TEST_HUB_BROWSER_AUTH_URL as string
-     : process.env.REACT_APP_TEST_HUB_BROWSER_AUTH_URL as string;
+     : process.env.REACT_APP_PROD_HUB_BROWSER_AUTH_URL as string;
 
     this.identity = await this.getIdentity();
     let buckets: Buckets;
